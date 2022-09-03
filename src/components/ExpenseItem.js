6067bac91253e5f894f3efc7765d348ce15a48eb
ajiajith001 <ajiajith001@users.noreply.github.com>
2022-09-03 16:40:10 +0530

    Added props to ExpensiveItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,17 +2,13 @@ import React from 'react';
 
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+function ExpenseItem({ date, title, amount }) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
