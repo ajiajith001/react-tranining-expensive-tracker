63ce248e4155236585fbd00ca5cd5ee724436288
ajiajith001 <ajiajith001@users.noreply.github.com>
2022-09-03 16:37:38 +0530

    moved the hardcoded values to const variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import './ExpenseItem.css';
 
-function ExpenseItem({ date, title, amount }) {
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
