6056eb51ca2056aabbd3a75d43491ffdfaf78105
ajiajith001 <ajiajith001@users.noreply.github.com>
2022-09-10 22:44:22 +0530

    Added useState example in ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem({ date, title, amount }) {
+function ExpenseItem({ date, title: titleProp, amount }) {
+  const [title, setTitle] = useState(titleProp);
+  console.log('ExpenseItem evaluated by React');
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -11,6 +19,7 @@ function ExpenseItem({ date, title, amount }) {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
